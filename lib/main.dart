8ea6bf719aee7baf8f5e8fa91286bfca44763298
Caetano-Ventura-Coutinho <currytano@gmail.com>
2024-06-14 16:58:58 -0300

    Adicionado a bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,27 @@ class MainApp extends StatelessWidget {
         theme: ThemeData(
             scaffoldBackgroundColor: const Color.fromARGB(255, 255, 250, 206)),
         home: Scaffold(
+
+          //AppBar
           appBar: AppBar(
             title: const Text('Cleany Beaches'),
             backgroundColor: const Color.fromARGB(255, 0, 186, 243),
             titleTextStyle: GoogleFonts.homenaje(fontSize: 32),
           ),
+
+          //NavigationBar
+          bottomNavigationBar: NavigationBar(
+            surfaceTintColor: const Color.fromARGB(255, 133, 226, 255),
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.home), label: 'Início'),
+              NavigationDestination(icon: Icon(Icons.groups), label: "ONG's"),
+              NavigationDestination(icon: Icon(Icons.location_on), label: 'Rotas'),
+              NavigationDestination(icon: Icon(Icons.info), label: 'Sobre')
+            ],
+            backgroundColor: const Color.fromARGB(255, 0, 186, 243),
+          ),
+          
+          //Body
           body: Center(
             child: Column(
               children: <Widget>[
@@ -41,9 +57,9 @@ class MainApp extends StatelessWidget {
                             style: GoogleFonts.getFont("Jost", fontSize: 16),
                           ),
                           TextButton.icon(
-                             style: TextButton.styleFrom(
-                              foregroundColor: Colors.black,
-                            ),
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.black,
+                              ),
                               onPressed: () {},
                               icon: const Icon(Icons.location_on),
                               label: const Text('Clique para localização')),
@@ -66,9 +82,9 @@ class MainApp extends StatelessWidget {
                             style: GoogleFonts.getFont("Jost", fontSize: 16),
                           ),
                           TextButton.icon(
-                             style: TextButton.styleFrom(
-                              foregroundColor: Colors.black,
-                            ),
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.black,
+                              ),
                               onPressed: () {},
                               icon: const Icon(Icons.location_on),
                               label: const Text('Clique para localização')),
@@ -80,15 +96,15 @@ class MainApp extends StatelessWidget {
 
                 //Botão ONG
                 Container(
-                  margin: const EdgeInsets.all(30),
+                  margin: const EdgeInsets.all(40),
                   child: SizedBox(
                     width: 293,
                     height: 84,
                     child: TextButton.icon(
                         onPressed: () {},
-                        icon: const Icon(Icons.group, size: 70),
-                        label: const Text("ONG's Saiba Mais", style: TextStyle(fontSize: 20)),
-                        
+                        icon: const Icon(Icons.groups_2, size: 70),
+                        label: const Text("ONG's Saiba Mais",
+                            style: TextStyle(fontSize: 20)),
                         style: TextButton.styleFrom(
                             foregroundColor: Colors.black,
                             backgroundColor: Colors.white,
@@ -112,9 +128,9 @@ class MainApp extends StatelessWidget {
                             style: GoogleFonts.getFont("Jost", fontSize: 16),
                           ),
                           TextButton.icon(
-                            style: TextButton.styleFrom(
-                              foregroundColor: Colors.black,
-                            ),
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.black,
+                              ),
                               onPressed: () {},
                               icon: const Icon(Icons.location_on),
                               label: const Text('Clique para localização')),
