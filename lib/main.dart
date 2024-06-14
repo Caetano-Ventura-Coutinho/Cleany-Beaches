bcc4e4e062e52de8c68c00d889598b7b2a15fc0c
Caetano-Ventura-Coutinho <currytano@gmail.com>
2024-06-14 10:34:04 -0300

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -11,44 +12,120 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: const Color.fromARGB(255, 255, 250 , 206)),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Cleany Beaches'),
-          backgroundColor: const Color.fromARGB(255, 0, 186, 243),
-          titleTextStyle: GoogleFonts.homenaje(fontSize: 32),
-        ),
-        body: Center(
-          
-          child: Column(
-            
-          children: <Widget> [
-            Text(
-              'Praias',
-              style: GoogleFonts.getFont("Jost", fontSize: 26),
-              ),
-             Row(
-            children: <Widget>[
-              Image.asset('img/imgPraiaF.png'),
-              Column(
-                children:  <Widget>[
-                  
-                  Text(
-              'Praia do Félix',
-              style: GoogleFonts.getFont("Jost", fontSize: 16),
-              ),
-              TextButton.icon(onPressed: () {}, icon: const Icon(Icons.location_on), label: const Text('Clique para localização') ),
+        theme: ThemeData(
+            scaffoldBackgroundColor: const Color.fromARGB(255, 255, 250, 206)),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Cleany Beaches'),
+            backgroundColor: const Color.fromARGB(255, 0, 186, 243),
+            titleTextStyle: GoogleFonts.homenaje(fontSize: 32),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Praias',
+                  style: GoogleFonts.getFont("Jost", fontSize: 26),
+                ),
 
-                ],
-              )
-              
-            ],      
-            )
-            ],
-        ),
-      ),
-      
-      )
-    );
+                //Praia do Félix
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset('img/imgPraiaF.png'),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Praia do Félix',
+                            style: GoogleFonts.getFont("Jost", fontSize: 16),
+                          ),
+                          TextButton.icon(
+                             style: TextButton.styleFrom(
+                              foregroundColor: Colors.black,
+                            ),
+                              onPressed: () {},
+                              icon: const Icon(Icons.location_on),
+                              label: const Text('Clique para localização')),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                //Praia do Lázaro
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset('img/imgPraiaL.png'),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Praia do Lázaro',
+                            style: GoogleFonts.getFont("Jost", fontSize: 16),
+                          ),
+                          TextButton.icon(
+                             style: TextButton.styleFrom(
+                              foregroundColor: Colors.black,
+                            ),
+                              onPressed: () {},
+                              icon: const Icon(Icons.location_on),
+                              label: const Text('Clique para localização')),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                //Botão ONG
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  child: SizedBox(
+                    width: 293,
+                    height: 84,
+                    child: TextButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.group, size: 70),
+                        label: const Text("ONG's Saiba Mais", style: TextStyle(fontSize: 20)),
+                        
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.white,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8))),
+                            side: const BorderSide(color: Colors.black))),
+                  ),
+                ),
+
+                //Praia da Enseada
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset('img/imgPraiaE.png'),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Praia da Enseada',
+                            style: GoogleFonts.getFont("Jost", fontSize: 16),
+                          ),
+                          TextButton.icon(
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.black,
+                            ),
+                              onPressed: () {},
+                              icon: const Icon(Icons.location_on),
+                              label: const Text('Clique para localização')),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
