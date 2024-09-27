ae473a13bb284958b3b2f6b5ec27cf0933c5e6dd
Daniel <bueno.dani.day@gmail.com>
2024-09-27 16:01:45 -0300

    corrigindo a classe praia

--- lib/Praia.dart
+++ lib/Praia.dart
@@ -6,6 +6,8 @@ class Praia
   final bool Condicao; 
   final String Rota; 
   final String LinkMaps; 
+  final String Mun; 
 
-  const Praia(this.Id, this.Nome, this.Img, this.Condicao, this.Rota, this.LinkMaps);
+
+  const Praia(this.Id, this.Nome, this.Img, this.Condicao, this.Rota, this.LinkMaps, this.Mun);
 }
